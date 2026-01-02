c756a9694156b03dddff602ffb373e2059bc4680
codenzadarin <darin@codenzasoft.ca>
2026-01-02 16:36:18 -0600

    optimize 1

--- src/main/java/com/codenzasoft/advent2025/day10/Toggler.java
+++ src/main/java/com/codenzasoft/advent2025/day10/Toggler.java
@@ -68,7 +68,8 @@ public class Toggler {
     if (combinationIndex < combinations.size()) {
       final ButtonCombination combination = combinations.get(combinationIndex);
       final int maxPresses = combination.getMaxAllowablePresses(machine);
-      for (int p = 1; p <= maxPresses; p++) {
+      final int currLevel = levels.levels().get(combinationIndex);
+      for (int p = currLevel; p <= maxPresses; p++) {
         final Iterator<List<Button>> iterator = combination.getCombinations(p);
         while (iterator.hasNext()) {
           total++;
